5da25cdc796ce15548b8080b88f535ebdb4d4098
Sergio Tapia <sergiotapia@outlook.com>
2015-05-02 18:34:35 -0400

    List upcoming auctions in the index page.

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,4 +1,6 @@
 class Auction < ActiveRecord::Base
+  default_scope { order('started_at ASC') } 
+
   belongs_to :product, touch: true
   has_many :bids
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
+  get 'subastas', to: 'auctions#index', as: 'auctions'
+  resources :auctions
   root 'home#index'
 end
